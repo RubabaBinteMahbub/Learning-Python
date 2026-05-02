31241bc0fc3c1e20445c829041377de65b9ac68e
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-05-02 13:49:26 +0600

    Create Sum.py

---
+++ Basic/Sum.py
@@ -0,0 +1,12 @@
+a = int(input("Enter the value of a : "))
+b = int(input("Enter the value of b : "))
+
+print(a+b)
+
+total=a+b
+print("Sum of numbers : ",total)
+
+# sum() ব্যবহার করতে হলে আগে যেগুলোর জন্য ব্যবহার
+করতে চাই সেগুলোর list বানাতে হবে।
+result = sum([a,b]);
+print("Sum of a and b : ",result)
