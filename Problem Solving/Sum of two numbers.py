31993297498eed4e2b7fd9b7b60e2fb816cbd4e0
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-05-02 13:51:17 +0600

    Add script for summing two numbers
    Implement a Python script to sum two user-input numbers and display the result.

---
+++ Problem Solving/Sum of two numbers.py
@@ -0,0 +1,10 @@
+a = int(input("Enter the value of a : "))
+b = int(input("Enter the value of b : "))
+
+print(a+b)
+
+total=a+b
+print("Sum of numbers : ",total)
+
+result = sum([a,b]);
+print("Sum of a and b : ",result)
