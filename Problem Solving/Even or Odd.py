dcb64ccadafe62abcd3e9efb92b500efba8743d3
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-05-02 14:20:11 +0600

    Add Even or Odd checker in Even or Odd.py

---
+++ Problem Solving/Even or Odd.py
@@ -0,0 +1,8 @@
+num = int(input("Enter the number : "))
+
+if num==0:
+    print(f"The number {num} is 0.")
+elif num%2==0:
+    print(f"The number {num} is even.")
+else:
+    print(f"The number {num} is odd.")
