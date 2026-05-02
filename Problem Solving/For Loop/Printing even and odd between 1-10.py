04028dec88b894d85eeb68621e3a1d76afadf208
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-05-02 14:51:08 +0600

    Add script to print even and odd numbers from 1 to 10
    Prints even and odd numbers between 1 and 10.

---
+++ Problem Solving/For Loop/Printing even and odd between 1-10.py
@@ -0,0 +1,12 @@
+# even numbers between 1 to 10
+for i in range(1,11):
+    if i % 2 == 0:
+        print(i,end=" ")
+        
+print()
+# print(\n) দিলে ২ টা new line print হবে।
+
+# odd numbers between 1 to 11
+for i in range(1,11):
+    if i % 2 != 0:
+        print(i,end=" ")
