ec341fbad8d61518ef7595fb9b94d73cb0391103
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-05-02 13:45:52 +0600

    Create Print.py for basic printing and input
    Add a basic Python script for printing and user input.

---
+++ Basic/Print.py
@@ -0,0 +1,18 @@
+# printing
+print("Rubaba Binte Mahbub")
+
+# using variable 
+name = "Rubaba"
+age = 20
+print(name)
+print(age)
+
+# user input (string)
+name = input("Enter your name : ")
+print("Hello, ", name)
+print("How are you, ",name + "?")
+
+# user input (number)
+a = int(input("Enter the value of a : "))
+b = int(input("Enter the value of b : "))
+print(a+b)
